a7e6a52d6a41e7e63891eeb723d398253cd5061e
risto <risto.kowaczewski@gmail.com>
2023-07-13 17:25:01 +0200

    Fixes for version 0.8.1

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 git rev-parse HEAD > app/uix/revision.txt
-docker build . --no-cache -t tracardi/com-microservice:0.8.0-dev
-docker push tracardi/com-microservice:0.8.0-dev
+docker build . --no-cache -t tracardi/com-microservice:0.8.1-rc3
+docker push tracardi/com-microservice:0.8.1-rc3
