86a53bf3fe8800265b1333c0a28ade926461a843
risto <risto.kowaczewski@gmail.com>
2023-12-20 10:17:35 +0100

    Microservice fix

--- app/main.py
+++ app/main.py
@@ -1,12 +1,3 @@
-from app.repo.services import repo
+import uvicorn
 
-
-# @repo.plugin(id="test")
-# def xxx(payload):
-#     print("insizde")
-#     return True
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("app.server:application", host="0.0.0.0", port=20000, log_level="info")
+uvicorn.run("app.server:application", host="0.0.0.0", port=20000, log_level="info")
